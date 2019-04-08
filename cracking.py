f9acf1325be3a89f393c3029bfa066e9e3bb016c
MR.51MB4H <48198896+kingcracker@users.noreply.github.com>
2019-04-08 17:12:44 +0700

    Add files via upload

---
+++ cracking.py
@@ -0,0 +1,85 @@
+import os
+import sys
+
+
+print ("╔═╗╔═╗╔══╗╔═╗╔╦╗")
+print ("║╔╝║╬║║╔╗║║╔╝║╔╝")
+print ("║╚╗║╗╣║╠╣║║╚╗║╚╗")
+print ("╚═╝╚╩╝╚╝╚╝╚═╝╚╩╝")
+
+
+
+
+
+print("Crete by: MR.51MB4H")
+print("hack wifi: ")
+print("______waiting_________")
+
+input ("nama wifi: ")
+print ("")
+
+
+
+
+
+
+print("sedang mencari info")
+
+print("")
+
+print("mengizinkan setup wifi")
+print("")
+
+
+print("[!] Berhasil Mendapat   Wifi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("Information Wifi")
+print("")
+
+
+
+
+
+
+
+
+print("Ip Address : 192.56.77.2")
+
+
+
+
+
+
+
+
+print("Password : modal cuk") 
+
+
+
+print("Wifi Security : WPA/WP2 PSK")
+print("_/\/\/\/\/\________dead...")
+
+print("")
+
+
+
+
+
+
+
+print("goblok lu")  
+
+print("thanks sudah pakai tools ini")
+print("MR.51MB4H")
